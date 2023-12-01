11ad0a1d75885f4dc9278c4feedd47e925363976
alic3 <alic3dev@gmail.com>
2023-12-01 14:11:36 -0500

    Add `Channel` functionality and analyser

--- src/Channel.ts
+++ src/Channel.ts
@@ -2,14 +2,48 @@ export class Channel {
   audioContext: AudioContext;
   output: AudioNode;
   gain: GainNode;
+  analyser: AnalyserNode;
+  // TODO: Add Effect Chain
+  destination: AudioNode;
+
+  #FFT_SIZE: number = 2048;
+  #ANALYSER_BUFFER_LENGTH: number;
+  #ANALYSER_DATA_ARRAY: Uint8Array;
 
   constructor(
     audioContext: AudioContext,
-    output: AudioNode = audioContext.destination
+    output: AudioNode = audioContext.destination,
+    withAnalyser: boolean = true
   ) {
     this.audioContext = audioContext;
     this.output = output;
+
     this.gain = this.audioContext.createGain();
-    this.gain.connect(this.output);
+
+    this.analyser = this.audioContext.createAnalyser();
+    this.analyser.fftSize = this.#FFT_SIZE;
+
+    if (withAnalyser) {
+      this.#ANALYSER_BUFFER_LENGTH = this.analyser.frequencyBinCount;
+      this.#ANALYSER_DATA_ARRAY = new Uint8Array(this.#ANALYSER_BUFFER_LENGTH);
+
+      this.analyser.connect(this.output);
+      this.gain.connect(this.analyser);
+    } else {
+      this.#ANALYSER_BUFFER_LENGTH = 0;
+      this.#ANALYSER_DATA_ARRAY = new Uint8Array(0);
+
+      this.gain.connect(this.output);
+
+      this.pollAnalyser = (): Uint8Array => this.#ANALYSER_DATA_ARRAY;
+    }
+
+    this.destination = this.gain;
+  }
+
+  pollAnalyser(): Uint8Array {
+    this.analyser.getByteTimeDomainData(this.#ANALYSER_DATA_ARRAY);
+
+    return this.#ANALYSER_DATA_ARRAY;
   }
 }
