6e0adcdc42600d06b6d380c10a6270141e3a07fb
alic3 <alic3dev@gmail.com>
2024-07-24 23:31:38 -0400

    dist -> build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['build', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
