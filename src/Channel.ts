5272767d8c9f368581887691221708ece692661b
alic3 <alic3dev@gmail.com>
2024-07-28 08:49:31 -0400

    Add `EffectChain` to `Channel`

--- src/Channel.ts
+++ src/Channel.ts
@@ -1,20 +1,25 @@
-import { Effect } from './Effect'
+import type { UUID } from 'crypto'
+
+import { BPMSync } from './BPMSync'
+import { EffectChain } from './EffectChain'
 
 export class Channel {
-  readonly audioContext: AudioContext
-  readonly output: AudioNode
-  readonly gain: GainNode
-  readonly analyser: AnalyserNode
-  readonly destination: AudioNode
-
-  #effectChain: Effect[] = []
-
-  readonly #FFT_SIZE: number = 2048
-  readonly #ANALYSER_BUFFER_LENGTH: number
-  readonly #ANALYSER_DATA_ARRAY: Uint8Array
-
-  public id: string = crypto.randomUUID()
+  public id: UUID = crypto.randomUUID()
   public name: string = 'Channel'
+
+  public readonly output: AudioNode
+  public readonly gain: GainNode
+  public readonly analyser?: AnalyserNode
+  public readonly destination: AudioNode
+
+  public readonly effectChain: EffectChain
+  public readonly BPMSync: BPMSync = new BPMSync({})
+  public readonly ANALYSER_BUFFER_LENGTH?: number
+  public readonly ANALYSER_DATA_ARRAY?: Uint8Array
+
+  private audioContext: AudioContext
+
+  static readonly FFT_SIZE: number = 2048
 
   constructor({
     id,
@@ -23,7 +28,7 @@ export class Channel {
     output = audioContext.destination,
     withAnalyser = true,
   }: {
-    id?: string
+    id?: UUID
     name?: string
     audioContext: AudioContext
     output?: AudioNode
@@ -43,53 +48,32 @@ export class Channel {
     this.gain = this.audioContext.createGain()
 
     this.analyser = this.audioContext.createAnalyser()
-    this.analyser.fftSize = this.#FFT_SIZE
+    this.analyser.fftSize = Channel.FFT_SIZE
 
     if (withAnalyser) {
-      this.#ANALYSER_BUFFER_LENGTH = this.analyser.frequencyBinCount
-      this.#ANALYSER_DATA_ARRAY = new Uint8Array(this.#ANALYSER_BUFFER_LENGTH)
+      this.ANALYSER_BUFFER_LENGTH = this.analyser.frequencyBinCount
+      this.ANALYSER_DATA_ARRAY = new Uint8Array(this.ANALYSER_BUFFER_LENGTH)
 
       this.analyser.connect(this.output)
       this.gain.connect(this.analyser)
     } else {
-      this.#ANALYSER_BUFFER_LENGTH = 0
-      this.#ANALYSER_DATA_ARRAY = new Uint8Array(0)
-
       this.gain.connect(this.output)
-
-      this.pollAnalyser = (): Uint8Array => this.#ANALYSER_DATA_ARRAY
     }
 
-    this.destination = this.gain
+    this.effectChain = new EffectChain({
+      audioContext,
+      BPMSync: this.BPMSync,
+      output: this.gain,
+    })
+
+    this.destination = this.effectChain.destination
   }
 
-  getEffectChain(): readonly Effect[] {
-    return Object.freeze([...this.#effectChain])
-  }
+  public pollAnalyser(): Uint8Array | undefined {
+    if (this.analyser && this.ANALYSER_DATA_ARRAY) {
+      this.analyser?.getByteTimeDomainData(this.ANALYSER_DATA_ARRAY)
+    }
 
-  addEffect(effect: Effect | typeof Effect) {
-    if (effect instanceof Effect) this.#effectChain.push(effect)
-    // else this.#effectChain.push(new effect(this.audioContext))
-  }
-
-  // moveEffect(effect: Effect, position: number | 'up' | 'down') {
-  //   const effectIndex = this.#effectChain.findIndex(chainedEffect => chainedEffect === effect);
-  //   this.#effectChain = []
-  // }
-
-  removeEffect(effect: Effect) {
-    const effectIndex = this.#effectChain.findIndex(
-      (chainedEffect) => chainedEffect === effect,
-    )
-
-    if (effectIndex === -1) throw new Error("Can't find effect to remove")
-
-    this.#effectChain.splice(effectIndex, 1)
-  }
-
-  pollAnalyser(): Uint8Array {
-    this.analyser.getByteTimeDomainData(this.#ANALYSER_DATA_ARRAY)
-
-    return this.#ANALYSER_DATA_ARRAY
+    return this.ANALYSER_DATA_ARRAY
   }
 }
