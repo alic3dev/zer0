b2031d6d360d95b6c74133fdfac35c889f12752e
alic3 <alic3dev@gmail.com>
2024-07-28 08:50:22 -0400

    Add `AutomatableParameter` to `DelayReverb`

--- src/effects/reverb/DelayReverb.ts
+++ src/effects/reverb/DelayReverb.ts
@@ -1,46 +1,74 @@
+import type { UUID } from 'crypto'
+
+import { AutomatableParameter } from '../../AutomatableParameter'
 import { Effect } from '../../Effect'
 
 export class DelayReverb extends Effect {
   audioContext: AudioContext
-  output: AudioNode
-  input: AudioNode
   delayNode: DelayNode
   decayGainNode: GainNode
-  dryGainNode: GainNode
-  wetGainNode: GainNode
-  delayTime: number = 60 / 270
+  delayTime: number = 1
   decayAmount: number = 1 / 16
-  mix: number = 1 / 3
 
-  constructor(
-    audioContext: AudioContext,
-    output: AudioNode = audioContext.destination,
-  ) {
-    super(audioContext, output)
+  constructor({
+    audioContext,
+    id,
+    name = 'Delay Reverb',
+    output = audioContext.destination,
+  }: {
+    audioContext: AudioContext
+    id?: UUID
+    name?: string
+    output?: AudioNode
+  }) {
+    super({ audioContext, output, id, name })
 
     this.audioContext = audioContext
     this.output = output
-    this.input = this.audioContext.createGain()
 
-    this.delayNode = this.audioContext.createDelay()
-    this.delayNode.delayTime.value = this.delayTime
+    this.delayNode = this.audioContext.createDelay(179)
+    this.setDelayTime()
 
     this.decayGainNode = this.audioContext.createGain()
     this.decayGainNode.gain.value = this.decayAmount
 
-    this.dryGainNode = this.audioContext.createGain()
-    this.dryGainNode.gain.value = 1 - this.mix
-    this.wetGainNode = this.audioContext.createGain()
-    this.wetGainNode.gain.value = this.mix
-
-    this.dryGainNode.connect(this.output)
-    this.input.connect(this.dryGainNode)
-
-    this.input.connect(this.delayNode)
+    this.destination.connect(this.delayNode)
     this.delayNode.connect(this.wetGainNode)
     this.delayNode.connect(this.decayGainNode)
     this.decayGainNode.connect(this.delayNode)
 
-    this.wetGainNode.connect(this.output)
+    this.BPMSync.onBPMChange((): void => {
+      this.delayNode.delayTime.value =
+        (60 * this.delayTime) / this.BPMSync.getUsableBPM()
+    })
+
+    this.BPMSync.onSyncChange((): void => {
+      this.delayNode.delayTime.value =
+        (60 * this.delayTime) / this.BPMSync.getUsableBPM()
+    })
+
+    this.parameters.push(
+      new AutomatableParameter<number>({
+        name: 'Delay Time',
+        getValue: (): number => this.delayTime,
+        setValue: (delayTime: number): void => {
+          this.setDelayTime(delayTime)
+        },
+      }),
+      new AutomatableParameter<number>({
+        name: 'Decay Amount',
+        getValue: (): number => this.decayAmount,
+        setValue: (decayAmount: number): void => {
+          this.decayGainNode.gain.value = decayAmount
+          this.decayAmount = decayAmount
+        },
+      }),
+    )
+  }
+
+  private setDelayTime(delayTime: number = this.delayTime): void {
+    this.delayTime = delayTime
+    this.delayNode.delayTime.value =
+      (60 * this.delayTime) / this.BPMSync.getUsableBPM()
   }
 }
